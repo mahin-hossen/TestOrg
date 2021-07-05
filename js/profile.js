1b331a8c175ee5a4bacfbf1406aa6cf8b91160b4
mahin-hossen <mahinhossen001@gmail.com>
2021-07-05 17:07:15 +0600

    profile done
    except css

---
+++ js/profile.js
@@ -0,0 +1,95 @@
+const name = document.querySelector("#name");
+const university = document.querySelector("#university");
+const department = document.querySelector("#department");
+const email = document.querySelector("#email");
+const birthday = document.querySelector("#birthday");
+const cgpa = document.querySelector("#cgpa");
+const edit = document.querySelector(".edit");
+const profile = document.querySelector(".profile");
+const addForm = document.querySelector("#addForm");
+const add = document.querySelector("#add");
+const save = document.querySelector("#save_button");
+
+const showUniversity = document.querySelector("#show_university");
+const showDepartment = document.querySelector("#show_department");
+const showBirthday = document.querySelector("#show_birthday");
+const showCgpa = document.querySelector("#show_cgpa");
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // const currUser = db.collection("usersinfo").doc(auth.currentUser.uid);
+    setProfile();
+  } else {
+    console.log(`hello`);
+  }
+});
+
+setProfile = async () => {
+  const currUser = await db
+    .collection("usersinfo")
+    .doc(auth.currentUser.uid)
+    .get();
+
+  window.currUser = currUser;
+
+  document.querySelector(".profile").style.display = "block";
+
+  name.textContent = currUser.data().username;
+  email.textContent = currUser.data().email;
+
+  console.log(showUniversity);
+
+  if (currUser.data().university) {
+    showUniversity.style.display = "block";
+    university.textContent = currUser.data().university;
+  }
+  if (currUser.data().department) {
+    showDepartment.style.display = "block";
+    department.textContent = currUser.data().department;
+  }
+  if (currUser.data().cgpa) {
+    showCgpa.style.display = "block";
+    cgpa.textContent = currUser.data().cgpa;
+  }
+  if (currUser.data().birthday) {
+    showBirthday.style.display = "block";
+    birthday.textContent = currUser.data().birthday;
+  }
+};
+
+edit.addEventListener("click", (e) => {
+  e.preventDefault();
+  profile.style.display = "none";
+  add.style.display = "block";
+  addForm["username"].value = currUser.data().username;
+  if (currUser.data().university)
+    addForm["university"].value = currUser.data().university;
+  if (currUser.data().department)
+    addForm["department"].value = currUser.data().department;
+  if (currUser.data().cgpa) addForm["cgpa"].value = currUser.data().cgpa;
+  if (currUser.data().birthday)
+    addForm["birthday"].value = currUser.data().birthday;
+});
+
+save.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const username = addForm["username"].value;
+  const university = addForm["university"].value;
+  const department = addForm["department"].value;
+  const cgpa = addForm["cgpa"].value;
+  const birthday = addForm["birthday"].value;
+
+  await db.collection("usersinfo").doc(auth.currentUser.uid).set(
+    {
+      username: username,
+      university: university,
+      department: department,
+      cgpa: cgpa,
+      birthday: birthday,
+    },
+    {
+      merge: true,
+    }
+  );
+  save.onclick = location.href = "../profile/index.html";
+});
--- js/signup.js
+++ js/signup.js
@@ -6,45 +6,48 @@ signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const email = signupForm["email"].value;
-  const username = signupForm["username"].value; 
+  const username = signupForm["username"].value;
+  // const university = signupForm["university"].value;
+  // const semester = signupForm["semester"].value;
+  const birthday = signupForm["birthday"].value;
   const password1 = signupForm["password1"].value;
   const password2 = signupForm["password2"].value;
-  const person = document.getElementsByName('person');
-   
+  const person = document.getElementsByName("person");
+
   if (password1 != password2) {
     password_mismatch.style.display = "block";
   } else {
-    auth.createUserWithEmailAndPassword(email, password1).then((cred) => {  
-      if(person[1].checked)      
-      {        
-        return db.collection("usersinfo").doc(cred.user.uid).set(
-        {
-          username: username,
-          email: email,
-          user_type: "student",
-          totalrooms: 0,
-        });
-      }
-      else if(person[0].checked)
-      {
-        return db.collection("usersinfo").doc(cred.user.uid).set(
-          {
+    auth
+      .createUserWithEmailAndPassword(email, password1)
+      .then((cred) => {
+        if (person[1].checked) {
+          return db.collection("usersinfo").doc(cred.user.uid).set({
+            username: username,
+            email: email,
+            // university: university,
+            // birthday: birthday,
+            user_type: "student",
+            totalrooms: 0,
+          });
+        } else if (person[0].checked) {
+          return db.collection("usersinfo").doc(cred.user.uid).set({
             username: username,
             email: email,
             user_type: "teacher",
+            // university: university,
+            // birthday: birthday,
             totalrooms: 0,
           });
-      }
-        
-    }).then(()=>{
-      signupForm.reset();
-      document.getElementById("button").onclick = location.href = "../login.html";  
-    });
+        }
+      })
+      .then(() => {
+        signupForm.reset();
+        document.getElementById("button").onclick = location.href =
+          "../login.html";
+      });
   }
 
   //   console.log(`${email}  ${username}  ${password1}  ${password2} `);
   //   if(teacher) console.log(`${teacher}`);
   //   else console.log(`${student}`);
 });
-
-
