8ae77edd358abef4b991643c2cbaa153a54f255d
mahin-hossen <mahinhossen001@gmail.com>
2021-07-05 22:05:39 +0600

    homepage done

---
+++ js/teacher.js
@@ -0,0 +1,14 @@
+const userID = document.querySelector("#userID");
+
+auth.onAuthStateChanged(async (user) => {
+  if (user) {
+    const currUser = await db
+      .collection("usersinfo")
+      .doc(auth.currentUser.uid)
+      .get();
+    userID.textContent = currUser.data().username;
+    console.log(currUser.data().username);
+  } else {
+    console.log(`hello`);
+  }
+});
