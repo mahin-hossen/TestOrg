305ee53242b3dfa6bf53214d6bca607b7d6494cf
mahin-hossen <mahinhossen001@gmail.com>
2021-07-06 14:05:25 +0600

    auth change

--- js/login.js
+++ js/login.js
@@ -2,11 +2,6 @@ const loginForm = document.querySelector("#login-form");
 // const googleButton = document.querySelector("#loginWithGoogle");
 const verification = document.querySelector("#verification");
 
-// let provider = new firebase.auth.GoogleAuthProvider();
-// provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
-// firebase.auth().languageCode = "it";
-// To apply the default browser preference instead of explicitly setting it.
-// firebase.auth().useDeviceLanguage();
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -22,19 +17,19 @@ loginForm.addEventListener("submit", (e) => {
         .then((doc) => {
           loginForm.reset();
           if (cred.user.emailVerified) {
-             if (doc.data().user_type == "student") {
-            document.getElementById("button").onclick = location.href =
-              "/Student/home_page/index.html";
-          } else {
-            document.getElementById("button").onclick = location.href =
-              "/Teacher/home_page/index.html";
-          }
-          }
-          else if(!cred.user.emailVerified)
-          {
+            if (doc.data().user_type == "student") {
+              document.getElementById("button").onclick = location.href =
+                "/Student/home_page/index.html";
+            } else {
+              document.getElementById("button").onclick = location.href =
+                "/Teacher/home_page/index.html";
+            }
+          } else if (!cred.user.emailVerified) {
             window.last_email = email;
-            document.querySelector("#last_part").style.display= "block";
-            document.querySelector("#msg").innerHTML = `You need to verify Your email first`
+            document.querySelector("#last_part").style.display = "block";
+            document.querySelector(
+              "#msg"
+            ).innerHTML = `You need to verify Your email first`;
           }
         });
     },
@@ -44,74 +39,10 @@ loginForm.addEventListener("submit", (e) => {
   );
 });
 
-verification.addEventListener("click",(e)=>
-{
+verification.addEventListener("click", (e) => {
   e.preventDefault();
   auth.currentUser.sendEmailVerification();
-})
-// googleButton.addEventListener("click", (e)=>
-// {
-//   firebase
-//   .auth()
-//   .signInWithPopup(provider)
-//   .then((result) => {
-//     console.log(result);
-//     // /** @type {firebase.auth.OAuthCredential} */
-//     // var credential = result.credential;
-
-//     // // This gives you a Google Access Token. You can use it to access the Google API.
-//     // var token = credential.accessToken;
-//     // // The signed-in user info.
-//     // var user = result.user;
-//     // ...
-//   })
-// });
-
-// function google() {
-
-//     .catch((error) => {
-//       // Handle Errors here.
-//       var errorCode = error.code;
-//       var errorMessage = error.message;
-//       // The email of the user's account used.
-//       var email = error.email;
-//       // The firebase.auth.AuthCredential type that was used.
-//       var credential = error.credential;
-//       // ...
-//     });
-
-//   // auth.signInWithPopup(provider).then((res) => {
-
-//   // firebase.auth()
-//   // .getRedirectResult()
-//   // .then((result) => {
-//   //   if (result.credential) {
-//   //     /** @type {firebase.auth.OAuthCredential} */
-//   //     var credential = result.credential;
-
-//   //     // This gives you a Google Access Token. You can use it to access the Google API.
-//   //     var token = credential.accessToken;
-//   //     // ...
-//   //   }
-//   //   // The signed-in user info.
-//   //   var user = result.user;
-//   // }).catch((error) => {
-//   //   // Handle Errors here.
-//   //   var errorCode = error.code;
-//   //   var errorMessage = error.message;
-//   //   // The email of the user's account used.
-//   //   var email = error.email;
-//   //   // The firebase.auth.AuthCredential type that was used.
-//   //   var credential = error.credential;
-//   //   // ...
-//   // });
-//   //   console.log(res);
-//   // });
-// }
-// /* googleButton.addEventListener("click",(e)=>
-// {
-
-// }) */
+});
 
 auth.onAuthStateChanged((user) => {
   if (user) {
